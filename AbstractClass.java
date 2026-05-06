d1f9cbd85d48d0e7b1b396690b61b56e0c56b6f7
saad12092003 <shaadbhuiyan2003@gmail.com>
2026-05-06 17:34:09 +0600

    ABstact

---
+++ AbstractClass.java
@@ -0,0 +1,29 @@
+abstract class animal
+{
+    abstract void sound();// Abstract method(Dosen't have any body)
+
+    void mortal() // normal method
+    {
+        System.out.println("Animal is mortal");
+    }
+}
+class Dog extends animal
+{
+    // Body of sound is provided here
+    void sound()
+    {
+        System.out.println("Animal makes Sound");
+    }
+}
+
+
+public class AbstractClass {
+    public static void main(String[] args) {
+        Dog d =new Dog();
+        d.mortal();
+        d.sound();
+
+
+    }
+}
+
