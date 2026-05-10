6fa4e8c1a2ccaac085192941f081e0d914f87f40
saad12092003 <shaadbhuiyan2003@gmail.com>
2026-05-10 18:02:47 +0600

    Up

---
+++ OOPfourPillarCombination.java
@@ -0,0 +1,71 @@
+
+abstract class Person {
+
+    protected String name;
+    protected int age;
+
+   Person(String name,int age)
+   {
+       this.name=name;
+       this.age=age;
+   }
+
+    abstract void display();
+}
+
+class child1 extends Person {
+
+    child1(String name,int age)
+    {
+        super(name,age);
+    }
+
+    public String getname() {
+        return this.name;
+    }
+
+    public int getage() {
+        return this.age;
+    }
+    
+    void display() {
+        System.out.println(getname());
+        System.out.println(getage());
+    }
+}
+
+class child2 extends Person {
+
+
+    child2(String name,int age)
+    {
+        super(name,age);
+    }
+
+    public String getname() {
+        return this.name;
+    }
+
+    public int getage() {
+        return this.age;
+    }
+
+    @Override
+    void display() {
+        System.out.println(getname());
+        System.out.println(getage());
+    }
+}
+
+public class OOPfourPillarCombination {
+
+    public static void main(String[] args) {
+
+
+        Person s1 = new child1("saad",23);
+        Person s2 = new child2("saadi",12);
+
+        s1.display();
+        s2.display();
+    }
+}
